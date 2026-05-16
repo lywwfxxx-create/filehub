5c75c1f1a3e9cc729119836f6914913628c3ba47
lywwfxxx-create <lywwfxxx@gmail.com>
2026-05-16 20:48:31 +0000

    v

--- app.py
+++ app.py
@@ -595,4 +595,7 @@ def internal_error(error):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    host = os.environ.get('HOST', '0.0.
+    host = os.environ.get('HOST', '0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host=host, port=port, debug=False)
+
