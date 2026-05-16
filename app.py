0bd746e07152ab12c9e636b5c166bcc00d243484
lywwfxxx-create <lywwfxxx@gmail.com>
2026-05-16 20:35:41 +0000

    g

--- app.py
+++ app.py
@@ -590,11 +590,13 @@ def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), 500
 
-
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     host = os.environ.get('HOST', '0.0.0.0')
     port = int(os.environ.get('PORT', 5000))
     app.run(host=host, port=port, debug=False)
+
+
+
 
